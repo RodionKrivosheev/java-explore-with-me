e1c23b4f142ffe4468e42ccbc5aca3cfad43b67c
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-09-23 16:02:56 +0300

    10 version - problem with main

--- main-service/src/main/java/ru/practicum/MainService.java
+++ main-service/src/main/java/ru/practicum/MainService.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApp {
+public class MainService {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApp.class, args);
+        SpringApplication.run(MainService.class, args);
     }
 }
